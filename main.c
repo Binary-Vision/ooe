697f878241af26bd11e2e273bcb4f040c01397b0
Binary-Vision <ooz6os2wqyky63ouos1muewc411bp3@gmail.com>
2020-12-27 22:54:41 +0000

    Temporary main file

--- main.c
+++ main.c
@@ -10,9 +10,30 @@
 #include <stdio.h>
 
 #include "types.h"
+#include "tty.h"
+#include <unistd.h>
+
+int roundt(float f)
+{
+    int round = (int)f;
+    if (f - (float)round >= 0.5)	round = (int)f + 1;
+    return round;
+}
 
 int main()
 {
-    printf("hello world\r\n");
+    Wins wins = wins_init();
+    wins_append_empty_win(&wins);
+    wins_append_empty_win(&wins);
+
+    wins.wins[0].ws.ws_row = 20;
+    wins.wins[0].ws.ws_col = 20;
+
+    wins.wins[0].ws.ws_row = 20;
+    wins.wins[0].ws.ws_col = 20;
+
+    printf("%ld;%ld;%d;%d\r\n", wins.wins_cap, wins.wins_size, wins.wins[0].ws.ws_row, wins.wins[0].ws.ws_col);
+
+    wins_free(&wins);
     return 0;
 }
