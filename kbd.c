e7a91242c7abd7c352b8e9fef4f4f0fff898dd00
Binary-Vision <ooz6os2wqyky63ouos1muewc411bp3@gmail.com>
2021-01-01 18:05:58 +0000

    Added keyboard inputting functionality and editor processing keys. This will be improved in the future

---
+++ kbd.c
@@ -0,0 +1,64 @@
+#include "kbd.h"
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define CTRL_KEY(k)	((k) & 0x1F)
+
+static int kbd_read_key() {
+  int nread;
+  char c;
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN) return -1;
+  }
+  if (c == '\x1b') {
+    char seq[3];
+    if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+    if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+    if (seq[0] == '[') {
+      if (seq[1] >= '0' && seq[1] <= '9') {
+        if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+        if (seq[2] == '~') {
+          switch (seq[1]) {
+            case '1': return HOME_KEY;
+            case '3': return DEL_KEY;
+            case '4': return END_KEY;
+            case '5': return PAGE_UP;
+            case '6': return PAGE_DOWN;
+            case '7': return HOME_KEY;
+            case '8': return END_KEY;
+          }
+        }
+      } else {
+        switch (seq[1]) {
+          case 'A': return ARROW_UP;
+          case 'B': return ARROW_DOWN;
+          case 'C': return ARROW_RIGHT;
+          case 'D': return ARROW_LEFT;
+          case 'H': return HOME_KEY;
+          case 'F': return END_KEY;
+        }
+      }
+    } else if (seq[0] == 'O') {
+      switch (seq[1]) {
+        case 'H': return HOME_KEY;
+        case 'F': return END_KEY;
+      }
+    }
+    return '\x1b';
+  } else {
+    return c;
+  }
+}
+
+void editor_kbd_proc_key()
+{
+    int key = kbd_read_key();
+
+    switch (key)
+    {
+    case CTRL_KEY('Q'):
+        exit(0);
+    }
+}
---
+++ kbd.h
@@ -0,0 +1,33 @@
+/*
+ *	Keyboard Input
+ *	Copyright
+ *		(C) 2020 Onyx
+ *
+ *	This file is part of OOE (Onyx`s Own Editor)
+ *
+ */
+
+#ifndef _OOE_KBD_H_
+#define _OOE_KBD_H_
+
+enum editorKeys
+{
+    KEY_BACKSPACE = 0x7F,
+    ESC_KEY = 0x1B,
+    ARROW_LEFT = 1000,
+    ARROW_RIGHT,
+    ARROW_UP,
+    ARROW_DOWN,
+    DEL_KEY,
+    HOME_KEY,
+    END_KEY,
+    PAGE_UP,
+    PAGE_DOWN
+};
+
+/*
+* Process kbd pressed keys relative to editor
+*/
+void editor_kbd_proc_key();
+
+#endif
