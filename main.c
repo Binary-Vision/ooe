816d60171684d10024877ea5869243733c3babcf
Binary-Vision <ooz6os2wqyky63ouos1muewc411bp3@gmail.com>
2021-01-01 18:06:40 +0000

    Can display 2 window bars!

--- main.c
+++ main.c
@@ -8,33 +8,47 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "types.h"
 #include "tty.h"
-#include <unistd.h>
+#include "kbd.h"
+#include "defs.h"
 
-int roundt(float f)
+Scrn* scrn_clean_ptr = NULL;
+
+void scrn_clean_exit()
 {
-    int round = (int)f;
-    if (f - (float)round >= 0.5)	round = (int)f + 1;
-    return round;
+    if (scrn_clean_ptr != NULL)
+        scrn_free(scrn_clean_ptr);
 }
 
 int main()
 {
     terminal_enable_raw_mode();
     Scrn scrn = scrn_empty();
+    atexit(scrn_clean_exit);
+
     terminal_get_window_size(&scrn.scrn_ws.ws_row, &scrn.scrn_ws.ws_col);
 
     wins_append_empty_win(&scrn.wins);
-    scrn.wins.wins[0].window_coord.y = 20;
-    scrn.wins.wins[0].window_coord.x = 0;
-    scrn.wins.wins[0].ws.ws_col = scrn.scrn_ws.ws_col / 2;;
-    
-    scrn_update(&scrn);
-    sleep(3);
+    wins_append_empty_win(&scrn.wins);
 
-    scrn_free(&scrn);
+    scrn.wins.wins[0].window_coord.y = 0;
+    scrn.wins.wins[0].window_coord.x = 0;
+    scrn.wins.wins[0].ws.ws_col = scrn.scrn_ws.ws_col / 2;
+
+    scrn.wins.wins[1].window_coord.y = 0;
+    scrn.wins.wins[1].window_coord.x = (scrn.scrn_ws.ws_col / 2);
+    scrn.wins.wins[1].ws.ws_col = round_whole((float)scrn.scrn_ws.ws_col / 2.0f);
+
+//    printf("%d;%d;%d\r\n", scrn.scrn_ws.ws_col, scrn.wins.wins[0].ws.ws_col, scrn.wins.wins[1].ws.ws_col);
+
+    while (1)
+    {
+        scrn_update(&scrn);
+        editor_kbd_proc_key();
+    }    
 
     return 0;
 }
