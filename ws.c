ef8a399dc63ddfe8034c16b975424109e6278927
Onyx-Official <OOZ6OS2wQykY63ouOS1mueWC411bp3@gmail.com>
2021-02-19 22:48:45 +0000

    Standard Commit

--- ws.c
+++ ws.c
@@ -17,8 +17,9 @@ int retIWin_scrnCursor(Wins* wins_ptr, const int cursor_y, const int cursor_x)
     for (size_t i = 0; i < wins_ptr->wins_size; i++)
     {
         // Bounds Checking
-        if (cursor_x >= wins_ptr->wins[i].window_coord.x + 1 && cursor_x <= wins_ptr->wins[i].window_coord.x + wins_ptr->wins[i].ws.ws_col && cursor_y >= wins_ptr->wins[i].window_coord.y + 1 && cursor_y < wins_ptr->wins[i].window_coord.y + wins_ptr->wins[i].ws.ws_col)
-            winIndex = i;
+        if (cursor_x >= wins_ptr->wins[i].window_coord.x + 1 && cursor_x <= wins_ptr->wins[i].window_coord.x + wins_ptr->wins[i].ws.ws_col)
+            if (cursor_y >= wins_ptr->wins[i].window_coord.y + 1 && cursor_y <= wins_ptr->wins[i].window_coord.y + wins_ptr->wins[i].ws.ws_row)
+                winIndex = i;
     }
 
     return winIndex;
